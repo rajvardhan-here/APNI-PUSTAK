42db64d41b697962ed9b1bc90ff53f808570b2e7
rajvardhan singh chouhan <166440682+rajvardhan-here@users.noreply.github.com>
2025-09-02 22:55:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (username && password) {
+      alert('Logged in successfully!');
+      window.location.href = 'home.html'; // Correct redirection after login
+    } else {
+      alert('Please enter both username and password');
+    }
+  }
+=======
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (username && password) {
+      alert('Logged in successfully!');
+      window.location.href = 'home.html'; // Correct redirection after login
+    } else {
+      alert('Please enter both username and password');
+    }
+  }
+>>>>>>> 51be3d7383b255e8d1941d26e6ee048c5a9c9972
+  
