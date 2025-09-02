ecad8906a1fe57cbc744c76f6c47ad3d65a73e84
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2025-09-03 01:49:50 +0530

    style.css changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # APNI-PUSTAK
 this is my 1st project of web dev
+
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -10,17 +9,3 @@ function login() {
       alert('Please enter both username and password');
     }
   }
-=======
-function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    if (username && password) {
-      alert('Logged in successfully!');
-      window.location.href = 'home.html'; // Correct redirection after login
-    } else {
-      alert('Please enter both username and password');
-    }
-  }
->>>>>>> 51be3d7383b255e8d1941d26e6ee048c5a9c9972
-  
